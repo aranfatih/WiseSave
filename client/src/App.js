99eed56ea95e44ad0a350e55c5559a440b586909
Akar <akarmathers13@gmail.com>
2019-09-06 15:12:05 +0300

    Add Tailwinds.

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Home from './home/home';
-import Profile from './profile/profile';
+
+import Home from './components/pages/Home';
 
 export default class App extends React.Component {
   render() {
@@ -10,7 +10,6 @@ export default class App extends React.Component {
       <BrowserRouter>
         <Switch>
           <Route exact path="/" component={Home} />
-          <Route exact path="/profile/:username" component={Profile}/>
         </Switch>
       </BrowserRouter>
     </div>
