bf7f9d5e8020eaa1c5c8e24eb4a06434702cb495
aran <othman.aran@gmail.com>
2019-09-06 17:35:04 +0300

    bug fix

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,20 @@
-import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Home from './home/home';
-import Profile from './profile/profile';
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Home from "./home/home";
+import Main from "./component/Main";
+import Profile from "./profile/profile";
 
 export default class App extends React.Component {
   render() {
-    return(
-    <div>
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/profile/:username" component={Profile}/>
-        </Switch>
-      </BrowserRouter>
-    </div>
-    )
+    return (
+      <div>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/" component={Main} />
+            <Route exact path="/profile/:username" component={Profile} />
+          </Switch>
+        </BrowserRouter>
+      </div>
+    );
   }
 }
---
+++ client/src/component/Main.js
@@ -0,0 +1,27 @@
+import AppBar from "@material-ui/core/AppBar";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import MenuIcon from "@material-ui/icons/Menu";
+import React, { Component } from "react";
+
+class Main extends Component {
+  render() {
+    return (
+      <div>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton edge="start" color="inherit" aria-label="menu">
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6">News</Typography>
+            <Button color="inherit">Login</Button>
+          </Toolbar>
+        </AppBar>
+      </div>
+    );
+  }
+}
+
+export default Main;
